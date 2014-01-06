f2b157a2f50d809c2f572abb7404763f68316b8f
diffiehellman64 <diffiehellman@mail.ru>
2014-01-06 14:20:01 +0400

    inta

--- menu.c
+++ menu.c
@@ -3,15 +3,10 @@
 #include <stdlib.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
-#define CTRLD 	4
-
-char *choices[] = {	"Create root CA",
-                        "Create intermediate CA",
-                        "Create certificate",
-                        "Exit"	};
 
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
 int get_max_elem(char *arr[], int count);
+int get_choice(char *choices[], int choices_count, char *title);
 
 int get_max_elem(char *arr[], int count)
 {
@@ -25,68 +20,82 @@ int get_max_elem(char *arr[], int count)
         return max_len;
 }
 
-/*int get_max_elem(char *arr[])
-{
-	int i;
-	int max_len = 0;
-	while (arr[i])
-	{
-		if (strlen(arr[i]) > max_len)
-			max_len = strlen(arr[i]);
-		++i;
-	}
-	return max_len;
-}*/
-
-void menu_render()
+int get_choice(char *choices[], int choices_count, char *title)
 {	ITEM **my_items;
 	MENU *my_menu;
         WINDOW *my_menu_win;
 	int c;	
         int n_choices, i, width;
         int row, col;
-	
+	int ch = 1;	
+	int ti;
+
 	getmaxyx(stdscr,row,col);
+	mvwprintw(stdscr, 4, 10, "row = %d | col = %d", row, col);
         cbreak();
         noecho();
-	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+//	init_pair(2, COLOR_YELLOW, COLOR_MAGENTA);
+//	init_pair(1, COLOR_MAGENTA, COLOR_CYAN);
+	init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
 
-        n_choices = ARRAY_SIZE(choices);				// вычисляем количество пунктов меню
-	width = get_max_elem(choices, n_choices);			// вычисляем размеры главного экрана
+        n_choices = choices_count;
+	width = get_max_elem(choices, n_choices);			// вычисляем размеры самой большой строки в массиве
+	
+	for (ti = 0; ti < n_choices; ++ti)
+	{
+		mvwprintw(stdscr, ti + 6, 10, "%d -> %d", ti, strlen(choices[ti]));
+	}
+
+	mvwprintw(stdscr, 5, 10, "width = %d ", width);
+
         my_items = (ITEM **)calloc(n_choices, sizeof(ITEM *));		// выделяем память
         for(i = 0; i < n_choices; ++i)
                 my_items[i] = new_item(choices[i], NULL);		// инициализируем пункты меню
 
 	my_menu = new_menu((ITEM **)my_items);				// создаем меню
 
-        my_menu_win = newwin(n_choices + 6, width + 9, row/3, (col - width)/2);	// создаем окно для меню
+        my_menu_win = newwin(n_choices + 6, width + 7, row - 40, (col - width)/2);	// создаем окно для меню
         keypad(my_menu_win, TRUE);
-     
+    	
 	/* Set main window and sub window */
         set_menu_win(my_menu, my_menu_win);
-        set_menu_sub(my_menu, derwin(my_menu_win, n_choices + 2, width + 3, 4, 2));
+        set_menu_sub(my_menu, derwin(my_menu_win, n_choices, width + 3, 4, 1));
         set_menu_mark(my_menu, " > ");					// Set menu mark to the string " > "
 
 	/* Print a border around the main window and print a title */
         box(my_menu_win, 0, 0);
-	print_in_middle(my_menu_win, 1, 0, width + 8, "Select action", COLOR_PAIR(1));
+	print_in_middle(my_menu_win, 1, 0, width + 4, title, COLOR_PAIR(1));
 	mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
-	mvwhline(my_menu_win, 2, 1, ACS_HLINE, width + 7);
-	mvwaddch(my_menu_win, 2, width + 8, ACS_RTEE);
-	mvprintw(LINES - 2, 0, "F1 to exit");
+	mvwhline(my_menu_win, 2, 1, ACS_HLINE, width + 5);
+	mvwaddch(my_menu_win, 2, width + 6, ACS_RTEE);
+	mvprintw(LINES - 2, 3, "press ENTER for choice action");
 	refresh();
         
 	/* Post the menu */
 	post_menu(my_menu);
 	wrefresh(my_menu_win);
 
-	while((c = wgetch(my_menu_win)) != KEY_F(1))
+	while((c = wgetch(my_menu_win)) != 10)
 	{       switch(c)
 	        {	case KEY_DOWN:
-				menu_driver(my_menu, REQ_DOWN_ITEM);
+				if (ch < choices_count)
+				{
+					menu_driver(my_menu, REQ_DOWN_ITEM);
+					mvprintw(3, 10, "                            ");
+					++ch;
+				}
 				break;
 			case KEY_UP:
-				menu_driver(my_menu, REQ_UP_ITEM);
+				if (ch > 1)
+				{
+					menu_driver(my_menu, REQ_UP_ITEM);
+					mvprintw(3, 10, "                            ");
+					--ch;
+				}
+				break;
+			case KEY_LEFT:
+				mvwprintw(stdscr, 3, 10, "select = %d", ch);
+				refresh();
 				break;
 		}
                 wrefresh(my_menu_win);
@@ -97,6 +106,7 @@ void menu_render()
         free_menu(my_menu);
         for(i = 0; i < n_choices; ++i)
                 free_item(my_items[i]);
+	return ch;
 }
 
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color)
@@ -122,14 +132,54 @@ void print_in_middle(WINDOW *win, int starty, int startx, int width, char *strin
 	refresh();
 }
 
+void create_ca(char *ca_name)
+{
+	
+}
+
+char *get_ca_name()
+{
+	char *name = malloc(10);
+	strcpy(name, "asdasdasdsadasd Ooo YEAH!!!");	
+	return name;
+}
+
 int main ()
 {
+	char *ca_name;
+	char *choices[] = {	"Create root CA",
+        	                "Create inwdsfsfsdfsdftermediate CA",
+        	                "123456789012345678901234567890",
+        	                "123456789012345678901234567890",
+        	               // "123456789012345678901234567890",
+        	                "Create intermediate CA",
+        	                "Crdsfsdfsdfsdfsdfsdfsdfsdfsdfsdfeate intermediate CA",
+                	        "Create certificate",
+                        	"Exit"	};
+
 	initscr();							// Initialize curses
 	start_color();
 	init_pair(2, COLOR_BLUE, COLOR_BLACK);
 	wattron(stdscr, COLOR_PAIR(2));
 	mvprintw(2, 10, "Graphic OpenSSL");
-	menu_render();
+	int ch = get_choice(choices, ARRAY_SIZE(choices), "Select action");
+	switch(ch)
+	{
+		case 1:
+			//printf("\n%s\n", "Create CA start...");
+			ca_name = get_ca_name();
+			break;
+		case 2:
+			ca_name = "ok";
+			break;
+		default:
+			ca_name = "ok!";
+			break;
+
+	}
 	endwin();
+	printf("Selected: %d (%s)\n", ch, choices[ch - 1]);
+	printf("CA name: %s\n", ca_name);
+	//free(ca_name);
 	return 0;
 }
